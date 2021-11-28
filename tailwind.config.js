c94ee700304a9e630bed73a54d73261dd0557c7e
Ethereal <ethereal97@gmail.com>
2021-11-28 08:26:31 +0000

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "src/**/**.{html,ts}"
+  ],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
