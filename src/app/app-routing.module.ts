9897f3f58cb2b39ce458b780b6d9f26f6a6e5170
mhawzay <94951789+mhawzay@users.noreply.github.com>
2021-11-28 15:20:55 +0630

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [
+  // { path: "", loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+];
